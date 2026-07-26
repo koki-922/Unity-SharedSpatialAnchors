8d1c0534a3fa4caaadb9bbd2d381e4a60e388d96
山田幸輝 <172130535+koki-922@users.noreply.github.com>
2026-07-26 21:49:56 +0900

    build in my PC

---
+++ Assets/Scripts/foo.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class foo: MonoBehaviour
+{
+    int b = 1;
+    void Start()
+    {
+        int b = 2;
+        Debug.Log(b);
+    }
+
+
+    void Update()
+    {
+        // Update logic here
+        int b = 3;
+
+    }
+}
